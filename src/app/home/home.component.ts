07bd5e3fd23eb9fb5222ccfa73a6cd48db21199f
Matteus Silva <matteus.silva@ccc.ufcg.edu.br>
2018-08-17 17:49:12 -0300

    Adiciona estrutura básica.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,32 @@ export class HomeComponent implements OnInit {
 
   titulo: string;
   metrica: string;
+  metricIcon: string;
+  auxilios: {}[];
 
   constructor() { }
 
+  setMetrica(value) {
+    this.metrica = value;
+  }
+
+  setIcon(value) {
+    this.metricIcon = value;
+  }
+
   ngOnInit() {
     this.titulo = 'O Preço do Martelo';
-    this.metrica = 'METRICA';
+    this.metrica = 'Moradias Populares';
+    this.metricIcon = 'fa fa-home fa-2x';
+    this.auxilios = [
+      {'nome':'Auxílio alimentação', 'valor': 7, 'icon': 'fa fa-cutlery'},
+      {'nome':'Auxílio moradia', 'valor': 10, 'icon': 'fa fa-building'},
+      {'nome':'Ajuda de custo', 'valor': 2, 'icon': 'fa fa-credit-card-alt '}
+    ];
+  }
+
+  genArray(n: number): any[] {
+    return Array(n);
   }
 
 }

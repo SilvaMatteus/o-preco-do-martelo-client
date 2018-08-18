35408ae981393f394c948dad8e21af64e31731f5
Matteus Silva <matteus.silva@ccc.ufcg.edu.br>
2018-08-18 16:02:51 -0300

    Up!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,7 @@ export class HomeComponent implements OnInit {
   metrica: string;
   metricIcon: string;
   auxilios: {}[];
+  credito: number;
 
   constructor() { }
 
@@ -26,6 +27,7 @@ export class HomeComponent implements OnInit {
     this.titulo = 'O Preço do Martelo';
     this.metrica = 'Moradias Populares';
     this.metricIcon = 'fa fa-home fa-2x';
+    this.credito = 75000;
     this.auxilios = [
       {'nome':'Auxílio alimentação', 'valor': 7, 'icon': 'fa fa-cutlery'},
       {'nome':'Auxílio moradia', 'valor': 10, 'icon': 'fa fa-building'},
@@ -35,6 +37,53 @@ export class HomeComponent implements OnInit {
 
   genArray(n: number): any[] {
     return Array(n);
+  }
+
+  getNumeroPossivel(preco) {
+    return this.credito / preco;
+  }
+
+  getProdutos() {
+    return [
+      {
+        'nome': 'Casas populares',
+        'preco': 73000
+      },
+      {
+        'nome': 'Ambulâncias',
+        'preco': 173000
+      },
+      {
+        'nome': 'Cestas básicas',
+        'preco': 200
+      },
+      {
+        'nome': 'Panetones',
+        'preco': 13
+      },
+      {
+        'nome': 'Panelas pra bater',
+        'preco': 45
+      }
+      ,
+      {
+        'nome': 'Champagne Chandon',
+        'preco': 76
+      },
+      {
+        'nome': 'Coxinhas',
+        'preco': 60000
+      },
+      {
+        'nome': ' Bolsas Chanel',
+        'preco': 314
+      }
+      ,
+      {
+        'nome': ' Alunos por ano',
+        'preco': 2500
+      }
+    ]
   }
 
 }
